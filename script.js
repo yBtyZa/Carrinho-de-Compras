2c4c02c4e76e7f7dfa39f9622a62750d43f07b7e
Guilherme Betsa <ybtyza@gmail.com>
2024-03-05 17:58:13 -0300

    Adicionando valor do carrinho

--- script.js
+++ script.js
@@ -3,35 +3,45 @@ document.getElementById("form").addEventListener("click", function (event) {
 });
 
 let carrinhoCompras = []
-let valorProduto = 0
 let carrinho = document.getElementById('carrinhoCompras')
+let total = document.getElementById('total')
+let somaDosValores = 0
+let valorProduto = 0
 
 function consultarValor() {
     let inputValue = document.getElementById('inputValue').value.toLowerCase()
     let consultarValorBtn = document.getElementById('consultarValorBtn')
     let comprarProdutoBtn = document.getElementById('comprarProdutoBtn')
 
-    if(inputValue === 'limao'){
+    if (inputValue === 'limao') {
         inputValue = 'limão'
     }
 
     switch (inputValue) {
         case 'morango':
             valorProduto = 5.99
+            respostaProduto(valorProduto)
             break
         case 'laranja':
             valorProduto = 3.99
+            respostaProduto(valorProduto)
             break
         case 'limão':
             valorProduto = 2.99
+            respostaProduto(valorProduto)
             break
         case 'manga':
             valorProduto = 4.99
+            respostaProduto(valorProduto)
             break
+        default:
+            alert('Produto não encontrado!')
     }
-    respostaProduto(valorProduto)
+    
+    if(valorProduto != 0){
     consultarValorBtn.style.display = 'none'
     comprarProdutoBtn.style.display = 'initial'
+    }
 }
 
 function respostaProduto(a) {
@@ -39,9 +49,9 @@ function respostaProduto(a) {
     respostaProduto.innerText = `O valor do produto é R$ ${a} Kg`
 }
 
-function comprarProduto(){
+function comprarProduto() {
     let inputValue = document.getElementById('inputValue').value.toLowerCase()
-    let novoProduto = {nome: inputValue, valor: valorProduto}
+    let novoProduto = { nome: inputValue, valor: valorProduto }
     carrinhoCompras.push(novoProduto)
 
     let consultarValorBtn = document.getElementById('consultarValorBtn')
@@ -51,12 +61,19 @@ function comprarProduto(){
     comprarProdutoBtn.style.display = 'none'
 
     atualizarCarrinho()
+
+    const respostaProduto = document.getElementById('respostaProduto')
+    respostaProduto.innerText = ''
 }
 
-function atualizarCarrinho(){
+function atualizarCarrinho() {
     let conteudoCarrinho = ''
+    somaDosValores = 0
     carrinhoCompras.forEach(element => {
         conteudoCarrinho += `${element.nome}: R$ ${element.valor} Kg <br>`
+        somaDosValores += parseFloat(element.valor)
     });
+
     carrinho.innerHTML = conteudoCarrinho
+    total.innerHTML = `Total: R$ ${somaDosValores.toFixed(2)}`
 }
